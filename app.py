ee6e13a983f2a86e0569fb2745adfec8b6c169b4
Keese Phillips <95185935+keesephillips@users.noreply.github.com>
2025-05-21 21:57:28 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.title("AIPI 561 AWS Bedrock")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("Question?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # Basic "Hello, World!" response
+    response = f"Response: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
